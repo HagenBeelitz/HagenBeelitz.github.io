4d532ac7d1faad1ea2bec3c153cc2937ae3e8fa9
hagenbeelitz <you@example.com>
2019-11-20 20:45:26 +0100

    Updates

---
+++ precache-manifest.11c05b0734c2997eda8030c741dcd191.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc2fbc848ed1b6098a3314b3ae207bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14623c5c256112c970c1",
+    "url": "/static/css/main.9181e811.chunk.css"
+  },
+  {
+    "revision": "3211ebbcfbc4772ab38a",
+    "url": "/static/js/2.38f4dfcf.chunk.js"
+  },
+  {
+    "revision": "14623c5c256112c970c1",
+    "url": "/static/js/main.0baceab3.chunk.js"
+  },
+  {
+    "revision": "a58fa6903d4b646cebc1",
+    "url": "/static/js/runtime-main.8102e0c6.js"
+  }
+]);
